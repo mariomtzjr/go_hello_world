63876c2aba48709935e01e1c8a82bd3109e7de39
Mario Martinez <francisco.smtz@gmail.com>
2023-06-14 20:07:48 -0600

    Initial commit.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example/go_hello_world
+
+go 1.20
---
+++ hello.go
@@ -0,0 +1,8 @@
+// This is a comment
+package main // A package is a way to group functions, and it's made up of all the files in the same directory
+
+import "fmt" // fmt package: contains functions for formatting text, including printing to the console
+
+func main() {
+	fmt.Println("Hello, World for first time!")
+}
